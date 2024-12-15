2398818f75bb1c430b93bb9c26b057835117060f
VinujaHansinduOfficial <vinujahansinduofficial@gmail.com>
2024-12-15 22:21:29 +0530

    .

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-console.log("bye")
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use("/",(req, res, next) =>{
+    res.send("It Is Working");
+})
+
+mongoose.connect("mongodb+srv://admin:WobJiFeRIDHibM9g@cluster0.64xm7.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
